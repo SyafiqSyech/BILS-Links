acb03b9da94f7b613545a014f5df8f8d601ab2e8
SyafiqSyech <106647971+SyafiqSyech@users.noreply.github.com>
2023-01-12 08:59:57 +0700

    pattern

--- java.js
+++ java.js
@@ -19,11 +19,12 @@ var logoBox = document.getElementById("logoBox");
 var patternBox = document.getElementById("patternBox");
 var contactBox = document.getElementById("contactBox");
 var contactText = document.getElementsByClassName("text");
+var patternRow = 6;
 
 box.addEventListener("scroll", function() {
-    logo.style.transform = "rotate("+box.scrollTop/8*-1+"deg)";
+    logo.style.transform = "rotate("+box.scrollTop/patternRow*-1+"deg)";
     logoBox.style.transform = "translateY("+box.scrollTop/2+"px)";
-    patternBox.style.transform = "translateY(200px) translateY("+(box.scrollTop/3)+"px)";
+    patternBox.style.transform = "translateY("+(box.scrollTop-((48+18)*patternRow)+30)+"px)";
     var max = box.scrollHeight - box.offsetHeight;
     contactBox.style.transform = "translateY("+(box.scrollTop-max+30)+"px)";
     if(max - 150 <= box.scrollTop){
@@ -35,16 +36,20 @@ box.addEventListener("scroll", function() {
             contactText[i].classList = "text text_hid";
         }
     }
-
+    
     const gridPatternBox = window.getComputedStyle(patternBox);
     const column = gridPatternBox.getPropertyValue("grid-template-columns").split(" ").length;
     const patterns = document.getElementsByClassName("pattern");
-    var deg = box.scrollTop/6
+    var deg = ((box.scrollTop/screen.height)*111)/2
     var j = 9;
+    var k = 0;
     for (let i = 0; i < patterns.length; i++) {
-        if(i%column == 0) j += -1;
-        if(i%column%2 == 0) patterns[i].style.transform = "rotate("+deg*-j+"deg)";
-        else patterns[i].style.transform = "translateY(20px) rotate("+deg*j+"deg)";
+        if(i%column == 0) j -= 1;
+        if(i%column == 0) k += 1;
+        if(i%column%4 == 0) patterns[i].style.transform = "translateY(20px) rotate("+deg*j+"deg)";
+        else if(i%column%4 == 1) patterns[i].style.transform = "rotate("+deg*k+"deg)";
+        else if(i%column%4 == 2) patterns[i].style.transform = "translateY(20px) rotate("+deg*-j+"deg)";
+        else patterns[i].style.transform = "rotate("+deg*-k+"deg)";
     }
 });
 
@@ -52,10 +57,12 @@ function background(){
     const gridPatternBox = window.getComputedStyle(patternBox);
     const column = gridPatternBox.getPropertyValue("grid-template-columns").split(" ").length;
     
-    for(var i = 0; i < column*8; i++){
+    for(var i = 0; i < column*patternRow; i++){
         const pattern = document.createElement("div");
-        if(i%column%2 == 0) pattern.classList = "pattern";
-        else pattern.classList = "pattern low";
+        const shape = document.createElement("div");
+        shape.classList = "shape";
+        pattern.classList = "pattern";
+        pattern.appendChild(shape);
         patternBox.appendChild(pattern);
     }
 }
